e34820499c66f6b0dd00c394c5ac055a0513485a
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2023-11-29 14:24:07 +0000

    initial commit

---
+++ softEng/notes.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "" << endl;
+
+    return 0;
+}
+
+/*
+WEEK 9 - SOFTWARE TESTING WOOOOOOOOOOOOOAAAAAAAAAH
+
+> to test means to validate whether the software meets its requirements
+or not
+    > if not met, see where and how the software doesn't meet the 
+    requirements
+
+> defect testing is trying to discover how the software can bug out/behave
+incorrectly
+
+> to debug means to find the errors in the code causing the software to
+fail and correct the code
+
+> activity 1:
+    > the tester (it's in the name) should test the program...
+    > ... as soon as possible, when the new code is deployed/commited
+
+
+
+> DIFFERENT TYPES OF DEVELOPMENT TESTING:
+> UNIT TESTING
+    > testing units, such as methods, functions and objects
+    > testing using frameworks, such as Catch and JUnit
+
+> COMPONENT TESTING
+    > testing components that interact with the software
+        > for example, returns behavioura/data throughout the software
+
+> SYSTEM TESTING
+    > combination, of previous two testings, it essentially tests the
+    whole program, or sometimes parts of the program
+    > the most time-consuming version of testing
+
+> RELEASE TESTING
+    > tests whether the final version of a software is ready to be released
+        > this means meeting requirements
+
+> USER TESTING
+    > where the target user gets to use the software you made for
+        > recieve feedback, create various versions of the software
+        and test again
+
+> AUTOMATED TESTING
+    > automate parts of the program
+        > repeat runs and checking the inputs/outputs, checking if the
+        results is correct/to be expected
+
+
+
+
+> CATCH2
+    > C++ ***UNIT TESTING*** framework
+        > https://github.com/catchorg/Catch2/tree/v2.x
+
+>
+*/
